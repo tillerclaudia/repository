e5a71569741eadfb3a3c050e42633227981f5c48
Rhiannan Wackes <rhiannanwackes@lawn-143-215-86-162.lawn.gatech.edu>
2019-09-16 14:39:59 -0400

    modified method in Person3.java

--- src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -4,7 +4,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  reversed string 
  *  
- *  @author  Bob
+ *  @author  Rhiannan Wackes
  *  @version 1.1
  */
 public class Person3 {
@@ -43,6 +43,10 @@ public class Person3 {
 	 */
 	private String calc(String input) {
 	  //Person 3 put your implementation here
-	  return null;
+		String reverse = "";
+		for (int ind = input.length(); ind--; ind == 0) {
+			reverse += input.charAt(ind);
+		}
+	  return reverse;
 	}
 }
